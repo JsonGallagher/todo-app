c5f4fd4c2ab0bd451d0b1d4adcd09eaf81492a92
Jason Gallagher <jasong13@gmail.com>
2020-04-14 14:45:11 -0600

    added todo object

--- todo-app.js
+++ todo-app.js
@@ -1,9 +1,26 @@
-const todos = document.querySelectorAll('p');
+const todos = [{
+    text: 'Order cat food',
+    completed: true
+}, {
+    text: 'Clean kitchen',
+    completed: false
+}, {
+    text: 'Buy food',
+    completed: true
+},{
+    text: 'Do work',
+    completed: false
+}, {    
+    text: 'Exercise',
+    completed: true
+}];
+
+const paragraphs = document.querySelectorAll('p');
 // Iterate over every p - remove every paragraph whos text contains “the”
 
-todos.forEach(function (todo) {
-    if (todo.textContent.toLowerCase().includes('the')) {
-        console.log(`Removed todo item: ${todo.textContent}`);        
-        todo.remove();
+paragraphs.forEach(function (paragraph) {
+    if (paragraph.textContent.toLowerCase().includes('the')) {
+        console.log(`Removed todo item: ${paragraph.textContent}`);        
+        paragraph.remove();
     }
 });
