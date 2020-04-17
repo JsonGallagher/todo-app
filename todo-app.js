24d0f5214da5657c36b6b9e030e8efa27172376e
Jason Gallagher <jasong13@gmail.com>
2020-04-17 11:31:11 -0600

    Corrected todo feature functionality

--- todo-app.js
+++ todo-app.js
@@ -15,11 +15,11 @@ const todos = [{
     completed: true
 }];
 
-// 1. Set up a div container for todos - DONE
-// 2. Setup filters (searchText)
 const filters = {
     searchText: ''
 }
+// 1. Set up a div container for todos - DONE
+// 2. Setup filters (searchText) - DONE
 // 3. Wire up a new filter input to change it
 document.querySelector('#search-text').addEventListener('input' , function (e) {
     filters.searchText = e.target.value;
@@ -31,40 +31,34 @@ const renderTodos = function (todos, filters) {
         return todo.text.toLowerCase().includes(filters.searchText.toLowerCase());
     })
 
+    // Print a summary message. 'You have 2 todos left' (store in a p element)
+    const incompleteTodos = filteredTodos.filter(function (todo) {
+        return !todo.completed;
+    });
+
     // Clear #todos div to wipe previously rendered notes on screen
     document.querySelector('#todos').innerHTML = '';
 
-    // Display only filtered notes
-    filteredTodos.forEach(function(todo) {
-        const todoEl = document.createElement('p');
-        todoEl.textContent = todo.text;
-        document.querySelector('div').appendChild(todoEl);
+    const summary = document.createElement('h2');
+    summary.textContent = `You have ${incompleteTodos.length} todos left to complete.`;
+    document.querySelector('#todos').appendChild(summary);
+
+    // Print a p for each todo above (use text value of the object as the visible value of the object)
+    filteredTodos.forEach(function (todo){
+    const p = document.createElement('p');
+    p.textContent = todo.text;
+    document.querySelector('#todos').appendChild(p);
     })
 }
 // Initial rendering of notes
 renderTodos(todos, filters);
 
-// Print a summary message. 'You have 2 todos left' (store in a p element)
-const incompleteTodos = todos.filter(function (todo) {
-        return !todo.completed;
-    });
+// Listen for new todo creation
+document.querySelector('#add-todo').addEventListener('click', function(e) {
+    console.log('Add a new todo');
+});
 
-const summary = document.createElement('h2');
-summary.textContent = `You have ${incompleteTodos.length} todos left.`;
-document.querySelector('body').appendChild(summary);
-
-// // Print a p for each todo above (use text value of the object as the visible value of the object)
-// todos.forEach(function (todo){
-//     const p = document.createElement('p');
-//     p.textContent = todo.text;
-//     document.querySelector('body').appendChild(p);
-// })
-// // Listen for new todo creation
-// document.querySelector('#add-todo').addEventListener('click', function(e) {
-//     console.log('Add a new todo');
-// });
-
-// // Listen for todo text input changes
-// document.querySelector('#new-todo').addEventListener('input', function(e) {
-//     console.log(e.target.value);
-// })
+// Listen for todo text input changes
+document.querySelector('#new-todo').addEventListener('input', function(e) {
+    console.log(e.target.value);
+})
